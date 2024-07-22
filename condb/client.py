eb938ab380c160faddec0e272cbf680e23c9cf6f
Vladimir Podstavkov <podstvkv@fnal.gov>
2024-07-22 14:23:41 -0500

    Cleanup

--- condb/client.py
+++ condb/client.py
@@ -1,18 +1,23 @@
-from .http_client import HTTPClient, ConDBError, ServerSideError, PermissionError, BadRequestError, NotFoundError, WebAPIError
-from condb.timelib import epoch
-import csv, io
-from urllib.parse import quote, unquote
-from requests.auth import HTTPDigestAuth
+import csv
+import io
 from collections import namedtuple
+from urllib.parse import quote
+
+from requests.auth import HTTPDigestAuth
+
+from condb.timelib import epoch
+
+# from .http_client import HTTPClient, ConDBError, ServerSideError, PermissionError, BadRequestError, NotFoundError, WebAPIError
+from .http_client import HTTPClient
 
 
 class ConDBClient(HTTPClient):
 
-    Version = "2.0.0"
-    
+    Version = "2.0.2"
+
     def __init__(self, url, username=None, password=None):
         """Initializes the ConDB client
-        
+
         Parameters
         ----------
             url : str
@@ -22,11 +27,11 @@ class ConDBClient(HTTPClient):
             password : str
                 Optional password
         """
-        
+
         HTTPClient.__init__(self, url)
         self.Username = username
         self.Password = password
-        
+
     def probe(self):
         out = self.get("probe")
         return out
@@ -36,7 +41,7 @@ class ConDBClient(HTTPClient):
         out["Client"] = self.Version
         return out
 
-    def get_data(self, folder, t0, t1=None, tag=None, tr=None, data_type=None, 
+    def get_data(self, folder, t0, t1=None, tag=None, tr=None, data_type=None,
                 channels=None, as_named_tuples=False):
         """Get data from folder
 
@@ -46,29 +51,29 @@ class ConDBClient(HTTPClient):
                 Folder name
             t0 : int or float
                 Tv to get data for
-            t1 : int or float 
+            t1 : int or float
                 Optional, if specified, the data will be returned for the t0...t1 time interval
             tag : str
                 Optional, return data from the tagged database state
-            tr : datetime or int or float 
+            tr : datetime or int or float
                 Optional, return data for previous state of the database identified with tr
             data_type : str
                 Optional, return sata for the given data source. Default: ""
             channels : list or tuple or int
-                If int, return data for single channel      
+                If int, return data for single channel
                 If tuple, return data for a range of channels, e.g.: (100, 151)
                 If list, return data for list of channels or channel ranges, e.g.: [10, 15, (23, 27)]
             as_named_tuples : bool
                 If true, return the data as collections.namedtuple's. Otherwise, native Python tuples
-        
+
         Returns
         -------
         tuple (columns, generator)
                 columns - list of column names
                 generator - producing tuples or collections.namedtuple's, one per data row
         """
-        
-        
+
+
         url = f"get?folder={folder}"
         if t1 is None:
             url += f"&t={t0}"
@@ -88,13 +93,15 @@ class ConDBClient(HTTPClient):
             for spec in channels:
                 if isinstance(spec, tuple):
                     c0, c1 = spec
-                    if c0 is None:  c0 = ''
-                    if c1 is None:  c1 = ''
+                    if c0 is None:
+                        c0 = ''
+                    if c1 is None:
+                        c1 = ''
                     ranges.append(f"{c0}-{c1}")
                 else:
                     ranges.append(str(spec))
             if ranges:
-                url += f"&channels=" + ",".join(ranges)
+                url += "&channels=" + ",".join(ranges)
 
         data = self.get_text(url)
         csv_buf = io.StringIO(data)
@@ -107,14 +114,17 @@ class ConDBClient(HTTPClient):
             for row in reader:
                 out_row = []
                 for x in row:
-                    try:    x = int(x)
-                    except:
-                        try:    x = float(x)
-                        except:
+                    try:
+                        x = int(x)
+                    except ValueError:
+                        try:
+                            x = float(x)
+                        except ValueError:
                             pass
                     out_row.append(x)
                 tup = tuple(out_row) if not as_named_tuples else tuple_type._make(out_row)
                 yield tup
+
         return columns, row_generator(reader, columns, as_named_tuples)
 
     def search_data(self, folder, tag=None, tr=None, data_type=None, channels=None,
@@ -128,12 +138,12 @@ class ConDBClient(HTTPClient):
                 Folder name
             tag : str
                 Optional, return data from the tagged database state
-            tr : datetime or int or float 
+            tr : datetime or int or float
                 Optional, return data for previous state of the database identified with tr
             data_type : str
                 Optional, return sata for the given data source. Default: ""
             channels : list or tuple or int
-                If int, return data for single channel      
+                If int, return data for single channel
                 If tuple, return data for a range of channels, e.g.: (100, 151)
                 If list, return data for list of channels or channel ranges, e.g.: [10, 15, (23, 27)]
             conditions : list of tuples
@@ -142,7 +152,7 @@ class ConDBClient(HTTPClient):
                 op is a string "<", "<=", "=", "!=", ">=", ">"
                 value is a string, boolean, numeric or None
             as_named_tuples : bool
-                If true, return the data as collections.namedtuple's. 
+                If true, return the data as collections.namedtuple's.
                 Otherwise, native Python tuples
 
         Returns
@@ -167,13 +177,15 @@ class ConDBClient(HTTPClient):
             for spec in channels:
                 if isinstance(spec, tuple):
                     c0, c1 = spec
-                    if c0 is None:  c0 = ''
-                    if c1 is None:  c1 = ''
+                    if c0 is None:
+                        c0 = ''
+                    if c1 is None:
+                        c1 = ''
                     ranges.append(f"{c0}-{c1}")
                 else:
                     ranges.append(str(spec))
             if ranges:
-                url += f"&channels=" + ",".join(ranges)
+                url += "&channels=" + ",".join(ranges)
 
         if conditions:
             # encode conditions
@@ -181,8 +193,8 @@ class ConDBClient(HTTPClient):
             for column, op, value in conditions:
                 if op not in ("<", "<=", "=", "!=", ">=", ">"):
                     raise ValueError(f"Unrecognized comparison operator: {op}")
-                if not isinstance(value, (int, float, str, bool)) and valie is not None:
-                    raise ValueError(f"Unsupported value type: %s" % (repr(value),))
+                if not isinstance(value, (int, float, str, bool)) and value is not None:
+                    raise ValueError("Unsupported value type: %s" % (repr(value),))
                 if value is None:
                     if op not in ("=", "!="):
                         raise ValueError(f"Unsupported operation {op} for comparison with NULL")
@@ -202,32 +214,36 @@ class ConDBClient(HTTPClient):
         csv_buf = io.StringIO(data)
         reader = csv.reader(csv_buf, delimiter = ",", quoting = csv.QUOTE_MINIMAL, lineterminator="\n")
         columns = next(reader)
+
         def row_generator(reader, columns, as_named_tuples):
             folder1 = folder.split('.')[-1]
             tuple_type = namedtuple(folder1, columns)
             for row in reader:
                 out_row = []
                 for x in row:
-                    try:    x = int(x)
-                    except:
-                        try:    x = float(x)
-                        except:
+                    try:
+                        x = int(x)
+                    except ValueError:
+                        try:
+                            x = float(x)
+                        except ValueError:
                             pass
                     out_row.append(x)
                 tup = tuple(out_row) if not as_named_tuples else tuple_type._make(out_row)
                 yield tup
+
         return columns, row_generator(reader, columns, as_named_tuples)
 
     def put_data(self, folder, data, columns, tr=None, data_type=None):
         """Add data to folder.
         Requires the username and password.
-        
+
         Parameters
         ----------
             folder : str
                 Folder name
             data : list
-                List of tuples: [channel, tv, ...]. 
+                List of tuples: [channel, tv, ...].
                 Order of columns after tv is the same as in ``columns``
             columns : list
                 Must begin with ["channel", "tv"]
@@ -236,20 +252,20 @@ class ConDBClient(HTTPClient):
             tr : datetime or int or float
                 Optional Tr to associate the data with. Default = current time
         """
-        
+
         if self.Username is None or self.Password is None:
             raise ValueError("Username and password are required")
-        
+
         if columns[0] != "channel" or columns[1] != "tv":
             raise ValueError("Channel and tv columns must be first and second in the columns list")
-        
+
         csv_buf = io.StringIO(newline="")
         writer = csv.writer(csv_buf, delimiter = ",", quoting = csv.QUOTE_MINIMAL, lineterminator="\n")
-        
+
         writer.writerow(columns)
         for row in data:
             writer.writerow(row)
-        
+
         url = f"put?folder={folder}"
         if tr is not None:
             tr = epoch(tr)
@@ -263,7 +279,7 @@ class ConDBClient(HTTPClient):
     def tag_state(self, folder, tag, tr=None, copy_from=None, override=False):
         """Add tag to a database state.
         Requires the username and password.
-        
+
         Parameters
         ----------
             folder : str
@@ -288,12 +304,12 @@ class ConDBClient(HTTPClient):
             url += f"&copy_from={copy_from}"
         if override:
             url += "&override=yes"
-        
+
         out = self.post_text(url, "", auth=HTTPDigestAuth(self.Username, self.Password))
         return float(out.strip())
 
-        
-        
-        
-        
+
+
+
+
 
