26e55b6e122de710e529e546e9a548e2218aefc7
Vladimir Podstavkov <podstvkv@fnal.gov>
2024-08-02 14:46:02 -0500

    Rename the module

--- condb/py3.py
+++
@@ -1,9 +0,0 @@
-def to_str(x):
-    if isinstance(x, bytes):
-        x = x.decode("utf-8")
-    return x
-
-def to_bytes(x):
-    if isinstance(x, str):
-        x = x.encode("utf-8")
-    return x
--- condb/version.py
+++
@@ -1,4 +0,0 @@
-Version = "2.0.3"
-
-if __name__ == "__main__":
-    print(Version)
